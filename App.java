5dc296572f6b32c5db1d44e256a7866e5d168c95
GurmeetSingh243 <49940956+GurmeetSingh243@users.noreply.github.com>
2025-04-02 17:41:59 +0000

    docker config

--- App.java
+++ App.java
@@ -2,6 +2,22 @@ public class App {
   
     public static void main(String[] args) {
         System.out.println("hello world");
+        int count = 0;
+        int[] arr = new int[]{1,1,0,1,0,1,0,1};
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] != 0 ){
+                arr[count] = arr[i];
+                count++;
+
+            }
+            while(count < arr.length){
+                arr[count] = 0;
+                count++;
+
+            }
+           
+
+        }
       
     }
   }
